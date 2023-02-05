9cdd5078637fa8908640546dda3d5a1116bb9e6e
LinnLinnHtun1197 <linlintun1197@gmail.com>
2023-02-05 08:30:56 -0500

    updated modal admin

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
+
+
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarMake
+    extra = 5
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
 
 # CarMakeAdmin class with CarModelInline
+class CarModelAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ('name', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['name', 'description']
 
 # Register models here
+admin.site.register(CarMake)
+admin.site.register(CarModel)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,14 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30, default='BMW 2023')
+    description = models.CharField(null=False, max_length=30, default='BMW 2023 is new model in 2023')
+    price = models.IntegerField(null=True)
+    
+    # Create a toString method for object string representation
+    def __str__(self):
+        return self.name + " " + self.description + " " + self.price
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,9 +27,21 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    # Many-To-Many relationship with CarMake
+    carmake = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=100, default='BMW')
+    dealerid = models.IntegerField(null=False)
+    type = models.CharField(max_length=200, default="Sedan")
+    year = models.DateField(null=True)
+
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+            "type: " + self.type
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+
